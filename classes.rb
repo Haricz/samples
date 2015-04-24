b80a04a721d3a5ffbaeaa3fb215fee24ee07bc3b
Haricz <johnpatricksuelto@yahoo.com>
2015-04-24 17:02:43 +0800

    I think this is working.

--- classes.rb
+++ classes.rb
@@ -1,14 +1,14 @@
 class String
   
   def ip_tobinary
-    a = "" 
+  a = "" 
 	ip = self.split(".")
 	ip.each { | x | a << "%08b" % x.to_i}
 	return a
   end
   
   def binary_toip
-  	a = ""
+  a = ""
 	ip = self.scan(/......../)
 	ip.each {|x| a << x.to_i(2).to_s + "."}
 	return a[0...-1]
@@ -44,7 +44,7 @@ def iprange_tolist(iprange)
   min = iprange[0].to_i(2)
   max = iprange[1].to_i(2)
   temp = (min..max).to_a
-  temp.each { | x | iplist << "%08b" % x }
+  temp.each { | x | iplist << "%032b" % x }
   return iplist
 end
 
@@ -61,26 +61,24 @@ def IPtoCIDR(iplist)
 
   length = iplist.length
 
-
   while index < length
-    if iplist[index][-4..-1].to_i == 0 
+    if iplist[index][-2..-1].to_i == 0 
       counter = index
       ip1 = iplist[index]
-        while index <= counter + 7 && index < length
-          index += 1          
+        while index < counter + 3 && index < length - 1
+          index += 1 
         end
       ip2 = iplist[index] 
       cidrlist << getprefix(ip1, ip2)
+      index += 1
 
-    elsif iplist[index][-4..-1].to_i == 1
+    elsif iplist[index][-2..-1].to_i == 1
     	ip1 = iplist[index]
-    	puts "I'm here"
         cidrlist << getprefix(ip1,ip1)
         index += 1
-      
 
-    elsif iplist[index][-4..-1].to_i == 10
-  	  if index + 1 < length &&  iplist[index + 1][-4..-1].to_i == 11
+    elsif iplist[index][-2..-1].to_i == 10
+  	  if index + 1 < length &&  iplist[index + 1][-2..-1].to_i == 11
   	  	ip1 = iplist[index]
   	  	ip2 = iplist[index + 1]
   	  	cidrlist << getprefix(ip1, ip2)
@@ -91,11 +89,11 @@ def IPtoCIDR(iplist)
   	    index += 1
   	  end
 
-  	elsif iplist[index][-4..-1].to_i == 11
+  	elsif iplist[index][-2..-1].to_i == 11
   	  ip1 = iplist[index]
   	  cidrlist << getprefix(ip1, ip1)
   	  index += 1  		
-    end   
+    end  
     counter = 0
   end  
   return cidrlist
@@ -104,14 +102,13 @@ end
 def getprefix(ip1, ip2)
   a = ip1
   b = ip2
-
   if ip1 == ip2
   	prefix = 32
   else
   	xor = (a.to_i(2) ^ b.to_i(2)).to_s(2) 
   	prefix = 32 - xor.length
   end
-  return prefix
+  return ip1.binary_toip << "/" << "#{prefix}"
 end
 
 
@@ -143,7 +140,7 @@ while input_count < 2
 end
 
 iplist = iprange_tolist(iprange)
-print iplist
+#puts iplist
 cidrlist = IPtoCIDR(iplist)
 
 puts cidrlist
